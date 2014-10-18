f01374f79490c7cb2ccdbe573d53846ef611b43b
Kazuhiro Ueha <ueha.kazuhiro@gmail.com>
2014-10-18 16:57:19 +0900

    ReadyStateHandlerの実装

--- src/request.js
+++ src/request.js
@@ -5,13 +5,26 @@
         return;
     }
 
-    function get(url){
+    function requestComplete(transport, options){
+        if(transport.status == 200){
+            if(typeof options.success == "function"){
+                options.success(transport);
+            }
+        }
+    }
+
+    function get(url, options){
         if (typeof url != "string"){
             throw new TypeError("URL Should be string");
         }
+        options = options || {};
         var transport = tddjs.ajax.create();
         transport.open("GET", url, true);
-        transport.onreadystatechange = function(){};
+        transport.onreadystatechange = function(){
+            if(transport.readyState == 4){
+                requestComplete(transport, options);
+            }
+        };
         transport.send();
     }
 
